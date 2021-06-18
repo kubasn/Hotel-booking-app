3e2a639ac6335828d7930b0fe6d85a4183a08753
Kuba Sosin <56204568+kubasn@users.noreply.github.com>
2021-06-18 13:26:15 +0200

    Add files via upload

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,21 +2,24 @@ import { useContext } from "react";
 import AuthContext from "../context/authContext";
 
 function useAuth() {
-    const authContext = useContext(AuthContext) //pobieram wartosc z kontekstu
-  const auth =authContext.isAuthenticated; //i teraz mozemy zwracac auth na podstawie authContext
+  const authContext = useContext(AuthContext); //pobieram wartosc z kontekstu
+  const auth = authContext.user; //i teraz mozemy zwracac auth na podstawie authContext
   //czyli pobieram wartosc z kontekstu
-  const setAuth = (value) => {
-        if(value){
-            authContext.login()
-        }else
-        {
-            authContext.logout();
-        }
-  }
+  const setAuth = user => {
+    if (user) {
+      authContext.login(user);
+      window.localStorage.setItem("token-data", JSON.stringify(user));
+      //localStorage-odpowiednik ciasteczek,zapisuje pod kluczem token-data te dane
+      //zapisanie do przegladarki tylko po to by w razie przelogowania miał cały czas te dane
+    } else {
+      authContext.logout();
+      window.localStorage.removeItem("token-data");
+      //po wylogowaniu usuwam token z localStorage(ciasteczek)
+    }
+  };
 
-    return [auth,setAuth];
+  return [auth, setAuth];
 }
-
 
 export default useAuth;
 //nazwa jest bardzo wazna,jeśli jest use to react wie że to jest hook,
