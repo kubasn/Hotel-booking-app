225518922be29d910445574dcf38021e072eeaac
Kuba Sosin <56204568+kubasn@users.noreply.github.com>
2021-07-08 11:12:12 +0200

    new commit

--- src/components/Hotels/Hotel/Hotel.js
+++ src/components/Hotels/Hotel/Hotel.js
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
-import styles from './Hotel.module.css';
-import hotelImg from '../../../assets/images/hotel.jpg'; 
-import PropTypes from 'prop-types';
-import ThemeContext from '../../../context/themeContext';
-import useAuth from '../../../hooks/useAuth';
-
+import React, { useContext } from "react";
+import styles from "./Hotel.module.css";
+import PropTypes from "prop-types";
+import ThemeContext from "../../../context/themeContext";
+import useAuth from "../../../hooks/useAuth";
+import { Link } from "react-router-dom";
 //pola wymagane
 const propTypes = {
   name: PropTypes.string.isRequired,
@@ -15,31 +14,32 @@ const propTypes = {
 };
 
 //const defaultProps = {
- //missing : 'to jest domyslny props'
+//missing : 'to jest domyslny props'
 //}
-
 
 function Hotel(props) {
   const theme = useContext(ThemeContext);
-  const [auth]=useAuth(); //jesli nie chce to nie musze pobierac drugiej wartosci setAuth
+  const [auth] = useAuth(); //jesli nie chce to nie musze pobierac drugiej wartosci setAuth
 
-const clickHandler = (e) => {
-e.preventDefault()  // na razie próbujemy zatrzymać działanie tego linku by się strona nie odświeżyła
-props.onOpen(props)
-}
-
-
+  const clickHandler = e => {
+    //e.preventDefault()  // na razie próbujemy zatrzymać działanie tego linku by się strona nie odświeżyła
+    if (props.onOpen) {
+      props.onOpen(props);
+    }
+  };
 
   return (
     <div className={`card ${styles.hotel}`}>
       <div className="card-body">
-      
         <div className="row">
           <div className="col-4">
             <img
-              src={hotelImg}
+              src={`https://placeimg.com/220/18${Math.floor(
+                Math.random() * 10
+              )}/arch`}
               alt=""
-              className="img-fluid img-thumbnail" />
+              className="img-fluid img-thumbnail"
+            />
           </div>
           <div className="col-8">
             <div className="row">
@@ -48,28 +48,24 @@ props.onOpen(props)
                 <span className="badge badge-light">{props.city}</span>
               </div>
               <div className="col text-right">
-                <h5>{props.rating}</h5>
-                <a href="#"
-                onClick={clickHandler}
-                className={`btn btn-${theme.color} mt-2 px-4`}>
-                Pokaż
-                </a>
+                <h5>Ocena: {props.rating ?? 0}</h5>
+
+                <Link onClick={clickHandler} to={`/hotele/${props.id}`}>
+                  Pokaż
+                </Link>
               </div>
             </div>
           </div>
-          
+
           <div className="col-12">
-            <p className={styles.description}>
-              {props.description}
-            </p>
-            {auth ? 
-              <p className='mt-2'> Dostepnosc: 4 pokoje</p>
-            : <p className='mt-2'> Dostepnosc: Zaloguj</p>
-            }
-           
+            <p className={styles.description}>{props.description}</p>
+            {auth ? (
+              <p className="mt-2"> Dostepnosc: {props.rooms}</p>
+            ) : (
+              <p className="mt-2"> Dostepnosc: Zaloguj</p>
+            )}
           </div>
         </div>
-
       </div>
     </div>
   );
--- src/components/Hotels/LastHotel/LastHotel.js
+++ src/components/Hotels/LastHotel/LastHotel.js
@@ -1,27 +1,40 @@
+import { Link } from "react-router-dom";
+
 const styles = {
-    color:'black'
-}
+  color: "black",
+};
 
-function LastHotel(props)
-{
-//const [hotel,setHotel] = useStateStorage(null);
+function LastHotel(props) {
+  //const [hotel,setHotel] = useStateStorage(null);
 
-//if(!props) return null;
-    return(
-        <div className="card mb-3 bg-light" >
-        <div className="card-header">
-        Ostatnio oglądana oferta, dalej zainteresowany ?</div>
-        <div className="card-body">
+  //if(!props) return null;
+  return (
+    <div className="card mb-3 bg-light">
+      <div className="card-header">
+        Ostatnio oglądana oferta, dalej zainteresowany ?
+      </div>
+      <div className="card-body">
         <div className="d-flex justify-content-between">
-        <h5 className="card-title">{props.name}</h5>
-        <span className="badge badge-light">{props.city}</span>
+          <h5 className="card-title">{props.name}</h5>
+          <span className="badge badge-light">{props.city}</span>
         </div>
-        <div className="ml-auto d-flex justify-content-between" style={{width:'100px'}} >
-        <a className=" btn btn-sm   btn-primary " href="#">Tak!</a>
-        <button className=" btn btn-sm  btn-danger " onClick={props.onRemove} >Nie!</button>
+        <div
+          className="ml-auto d-flex justify-content-between"
+          style={{ width: "100px" }}
+        >
+          <Link
+            to={`/hotele/${props.id}`}
+            className=" btn btn-sm   btn-primary "
+            href="#"
+          >
+            Tak!
+          </Link>
+          <button className=" btn btn-sm  btn-danger " onClick={props.onRemove}>
+            Nie!
+          </button>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 export default LastHotel;
